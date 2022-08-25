39d8fac734f48eec6881209094195fff3f88d8a5
bkomadina <bartol.komadina@gmail.com>
2022-08-25 18:37:12 +0200

    Remove useCreateIndex connect option

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const dbName = 'monsters';
 
 const monstersRoutes = require('./routes/monsters');
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Connected to the database'));
