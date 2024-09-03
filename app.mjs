e8272faed4c5ff0d0b9fb2cfedae186e908accf0
bkomadina <bartol.komadina@gmail.com>
2024-09-03 12:37:55 +0200

    Enable cors for local route

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import { config } from 'dotenv';
 import helmet from 'helmet';
+import cors from 'cors';
 import express, { json } from 'express';
 import monstersRoutes from './routes/monsters.mjs';
 
@@ -7,7 +8,13 @@ config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const corsOptions = {
+  // Allowed origins
+  origin: 'http://localhost:1234',
+  methods: 'GET,POST',
+};
 
+app.use(cors(corsOptions));
 app.use(json());
 app.use(helmet());
 app.use(monstersRoutes);
@@ -15,13 +22,6 @@ app.use(monstersRoutes);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
 });
 
 app.listen(port, () => {
