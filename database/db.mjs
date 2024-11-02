bee896b33780d346fcd8c66d379f4e1c67dee3c9
bkomadina <bartol.komadina@gmail.com>
2024-11-02 13:37:44 +0100

    Add hosted db url

--- database/db.mjs
+++ database/db.mjs
@@ -3,20 +3,26 @@ import { config } from "dotenv";
 
 config();
 
-const sql = postgres({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const sql =
+//   process.env.NODE_ENV === "production"
+//     ? postgres(process.env.DB_URL)
+//     : postgres({
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME,
+//       });
+const sql = postgres(process.env.DB_URL);
 
-console.log('Attempting to connect to the database...');
+console.log("Attempting to connect to the database...");
 
-sql`SELECT 1`.then(() => {
-    console.log('Database connection successful.');
-}).catch(err => {
-    console.error('Database connection error:', err);
-});
+sql`SELECT 1`
+  .then(() => {
+    console.log("Database connection successful.");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
 export default sql;
